442b03e3da89e664084b87650c0102b30f9de2e3
DarkMK69 <egor2006biryukov@yandex.ru>
2025-05-22 22:01:22 +0300

    добавил роутеры

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.db.session import engine, Base
-from app.api.v1 import books, auth
+from app.api.v1 import books, readers, borrows, auth
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
@@ -12,3 +12,5 @@ async def startup():
 
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["auth"])
 app.include_router(books.router, prefix="/api/v1/books", tags=["books"])
+app.include_router(readers.router, prefix="/api/v1/readers", tags=["readers"])
+app.include_router(borrows.router, prefix="/api/v1/borrows", tags=["borrows"])
